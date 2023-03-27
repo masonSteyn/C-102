cb26e359dd6374799d7620c8dcab59bfe8fbe516
masonSteyn <102371442+masonSteyn@users.noreply.github.com>
2023-03-27 15:40:59 -0500

    Add files via upload

---
+++ Camera2.py
@@ -0,0 +1,37 @@
+import cv2
+import dropbox
+import time
+import random
+
+start_time = time.time()
+
+def take_snapshot():
+    number = random.rawint(0,100)
+    videoCaptureObject = cv2.videocapture(0)
+    result = True
+    while(result):
+        ret,frame = videoCaptureObject.read()
+        img_name = "img"+str(number)+".png"
+        cv2.imwrite(img_name, frame)
+        start_time = time.time
+        result = False
+    return img_name
+    print("snapshot taken")
+    videoCaptureObject.release()
+    cv2.destroyAllwindows()
+
+def upload_file(img_name):
+    access_token = ""
+    file =img_name
+    file_from = file
+    file_to="/testFolder/"+(img_name)
+    dbx = dropbox.Dropbox(access_token)
+    with open(file_from, 'rb') as f:
+        dbx.files_upload(f.read(), file_to,mode=dropbox.files.Writemode.overwrite)
+        print("file uploaded")
+def main():
+    while(True):
+        if((time.time()-start_time) >=5 ):
+            name = take_snapshot()
+            upload_file(name)
+main()
